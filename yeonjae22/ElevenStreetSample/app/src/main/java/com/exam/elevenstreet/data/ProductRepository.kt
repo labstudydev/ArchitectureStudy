625a23061999cec91f3df117db4e689b46088456
LEEYEONJAE <eunoiaa22@gmail.com>
2019-09-18 03:03:54 +0900

    AS-3

---
+++ yeonjae22/ElevenStreetSample/app/src/main/java/com/exam/elevenstreet/data/ProductRepository.kt
@@ -0,0 +1,28 @@
+package com.exam.elevenstreet.data
+
+class ImageRepository private constructor() {
+//
+//    private val productRemoteDataSource: ProductRemoteDataSource
+//
+//    init {
+//        productRemoteDataSource = ProductRemoteDataSource()
+//    }
+//
+//    fun getImageItems(keyWord:String) {
+//        productRemoteDataSource.getSearchByKeyword(keyWord)
+//    }
+//
+//    companion object {
+//
+//        private var productLocalDataSource: ProductLocalDataSource? = null
+//
+//        val instance: ProductLocalDataSource
+//            get() {
+//                if (productLocalDataSource == null) {
+//                    productLocalDataSource = ProductLocalDataSource()
+//                }
+//
+//                return productLocalDataSource!!
+//            }
+//    }
+}
